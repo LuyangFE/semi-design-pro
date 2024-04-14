58e4d484df137631fcc00d508a954dcf1643cb7e
luyang <luyang@tezign.com>
2024-04-14 14:06:37 +0800

    feat: vchat

---
+++ src/pages/analysis/components/LineChart.tsx
@@ -0,0 +1,59 @@
+// 折线图
+import React, { useRef } from 'react';
+import { LineChart as LineChartComp, Line, Legend, Axis } from '@visactor/react-vchart';
+
+export const LineChart = (props: { height: number }) => {
+  const chartRef = useRef(null);
+  const handleChartClick = () => {
+    console.log('图表被点击了');
+  };
+
+  const barData = [
+    { type: 'Autocracies', year: '1930', value: 129 },
+    { type: 'Autocracies', year: '1940', value: 133 },
+    { type: 'Autocracies', year: '1950', value: 130 },
+    { type: 'Autocracies', year: '1960', value: 126 },
+    { type: 'Autocracies', year: '1970', value: 117 },
+    { type: 'Autocracies', year: '1980', value: 114 },
+    { type: 'Autocracies', year: '1990', value: 111 },
+    { type: 'Autocracies', year: '2000', value: 89 },
+    { type: 'Autocracies', year: '2010', value: 80 },
+    { type: 'Autocracies', year: '2018', value: 80 },
+    { type: 'Democracies', year: '1930', value: 22 },
+    { type: 'Democracies', year: '1940', value: 13 },
+    { type: 'Democracies', year: '1950', value: 25 },
+    { type: 'Democracies', year: '1960', value: 29 },
+    { type: 'Democracies', year: '1970', value: 38 },
+    { type: 'Democracies', year: '1980', value: 41 },
+    { type: 'Democracies', year: '1990', value: 57 },
+    { type: 'Democracies', year: '2000', value: 87 },
+    { type: 'Democracies', year: '2010', value: 98 },
+    { type: 'Democracies', year: '2018', value: 99 }
+  ];
+
+  return (
+    <div className='h-full'>
+      <LineChartComp height={props.height} ref={chartRef} data={[{ id: 'id0', values: barData }]} onClick={handleChartClick} type={'line'}>
+        <Line
+          seriesField="type"
+          xField={['year', 'type']}
+          yField="value"
+          line={{
+            style: {
+              stroke: '#000',
+              strokeWidth: 1
+            },
+            state: {
+              hover: {
+                stroke: 'black'
+              }
+            }
+          }}
+        />
+        <Axis orient="bottom" type="band" />
+        <Axis orient="left" max={200} type="linear" />
+        <Legend visible={true} position="start" orient="top" padding={{ bottom: 12 }} />
+      </LineChartComp>
+    </div>
+  );
+};
--- src/pages/analysis/index.tsx
+++ src/pages/analysis/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Col, Row } from '@douyinfe/semi-ui';
+import { LineChart } from './components/LineChart';
+import { Histogram } from './components/Histogram';
+import { AreaChart } from './components/AreaChart';
 
 const Analysis = () => {
-
   return (
     <div>
-      Analysis
+      <Row gutter={8}>
+        <LineChart height={500} />
+      </Row>
+      <Row gutter={8}>
+        <Col span={12}>
+          <Histogram height={400} />
+        </Col>
+        <Col span={12}>
+          <AreaChart height={400} />
+        </Col>
+      </Row>
     </div>
   );
 };
