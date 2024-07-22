370a9a9475c20d796ec98a5a03c7fa7a53a8b178
luyang <luyang@tezign.com>
2024-07-22 18:29:03 +0800

    feat： add openai

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,13 @@ export default defineConfig({
     }
   },
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.f2gpt.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
